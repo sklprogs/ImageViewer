aa17979bc4aef66555201e3b21ab8ccf48053add
skl.progs <skl.progs@gmail.com>
2017-12-26 02:06:49 +0300

    Basic

---
+++ image_viewer.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python3
+
+import gettext, gettext_windows
+gettext_windows.setup_env()
+gettext.install('shared','./locale')
+
+import tkinter as tk
+from PIL import Image as ig
+from PIL import ImageTk as it
+import shared as sh
+import sharedGUI as sg
+
+
+# How to combine frames, scrollbars and canvas
+class ImageViewer:
+    
+    def __init__(self,parent_obj,picture=None):
+        self._size      = 0
+        self._picture   = picture
+        self.parent_obj = parent_obj
+        self.gui()
+        self.pic()
+        # Set the scrolling region only after setting a picture
+        self.scrollregion()
+        self.scroll2start()
+        
+    def gui(self):
+        self.obj = sg.objs.new_top()
+        self.title()
+        self.frames()
+        self.canvas = sg.Canvas(parent_obj = self.frame1)
+        self.label  = sg.Label (parent_obj = self.frame1
+                               ,expand     = True
+                               ,fill       = 'both'
+                               )
+        self.canvas.embed(self.label)
+        self.scroll_x()
+        self.scroll_y()
+        self.canvas.focus()
+        self.bindings()
+        
+    def title(self,arg=None):
+        if not arg:
+            arg = 'Image:'
+        self.obj.title(arg)
+    
+    def scroll_x(self):
+        self.xscroll = tk.Scrollbar (master = self.frame_x.widget
+                                    ,orient = tk.HORIZONTAL
+                                    )
+        self.xscroll.pack (expand = True
+                          ,fill   = 'x'
+                          )
+        self.canvas.widget.config(xscrollcommand=self.xscroll.set)
+        self.xscroll.config(command=self.canvas.widget.xview)
+    
+    def scroll_y(self):
+        self.yscroll = tk.Scrollbar(master=self.frame_y.widget)
+        self.yscroll.pack (expand = True
+                          ,fill   = 'y'
+                          )
+        self.canvas.widget.config(yscrollcommand=self.yscroll.set)
+        self.yscroll.config(command=self.canvas.widget.yview)
+        
+    def frames(self):
+        self.frame   = sg.Frame (parent_obj = self.obj)
+        self.frame_x = sg.Frame (parent_obj = self.frame
+                                ,expand     = False
+                                ,fill       = 'x'
+                                ,side       = 'bottom'
+                                )
+        self.frame_y = sg.Frame (parent_obj = self.frame
+                                ,expand     = False
+                                ,fill       = 'y'
+                                ,side       = 'right'
+                                )
+        # This frame must be created after the bottom frame
+        self.frame1  = sg.Frame (parent_obj = self.frame)
+    
+    def pic(self):
+        if self._picture:
+            if sh.File(file=self._picture).Success:
+                loader = ig.open(self._picture)
+                self._size = loader.size
+                image = it.PhotoImage(loader)
+                self.label.widget.config(image=image)
+                # This prevents the garbage collector from deleting the image
+                self.label.widget.image = image
+            else:
+                sh.log.append ('ImageViewer.pic'
+                              ,_('WARNING')
+                              ,_('Wrong input data!')
+                              )
+        else:
+            sh.log.append ('ImageViewer.pic'
+                          ,_('WARNING')
+                          ,_('Empty input is not allowed!')
+                          )
+            
+    def scrollregion(self):
+        if self._size:
+            x, y = self._size
+            self.canvas.widget.configure (scrollregion = (-x/2,-y/2
+                                                         , x/2, y/2
+                                                         )
+                                         )
+        else:
+            sh.log.append ('ImageViewer.scrollregion'
+                          ,_('WARNING')
+                          ,_('Empty input is not allowed!')
+                          )
+                          
+    def scroll2start(self,*args):
+        self.canvas.widget.xview_moveto(0)
+        self.canvas.widget.yview_moveto(0)
+        
+    def bindings(self):
+        sg.bind (obj      = self.obj
+                ,bindings = ['<Control-q>','<Control-w>','<Escape>']
+                ,action   = self.close
+                )
+        sg.bind (obj      = self.obj
+                ,bindings = '<Down>'
+                ,action   = self.move_down
+                )
+        sg.bind (obj      = self.obj
+                ,bindings = '<Up>'
+                ,action   = self.move_up
+                )
+        sg.bind (obj      = self.obj
+                ,bindings = '<Left>'
+                ,action   = self.move_left
+                )
+        sg.bind (obj      = self.obj
+                ,bindings = '<Right>'
+                ,action   = self.move_right
+                )
+        sg.bind (obj      = self.obj
+                ,bindings = '<Next>'
+                ,action   = self.move_bottom
+                )
+        sg.bind (obj      = self.obj
+                ,bindings = '<Prior>'
+                ,action   = self.move_top
+                )
+                
+    def move_up(self,*args):
+        self.canvas.widget.yview_scroll(-1,'units')
+    
+    def move_down(self,*args):
+        self.canvas.widget.yview_scroll(1,'units')
+        
+    def move_left(self,*args):
+        self.canvas.widget.xview_scroll(-1,'units')
+        
+    def move_right(self,*args):
+        self.canvas.widget.xview_scroll(1,'units')
+        
+    def move_bottom(self,*args):
+        self.canvas.widget.yview_moveto('1.0')
+        
+    def move_top(self,*args):
+        self.canvas.widget.yview_moveto(0)
+
+    def show(self,*args):
+        self.obj.show()
+        
+    def close(self,*args):
+        self.obj.close()
+
+
+if __name__ == '__main__':
+    sg.objs.start()
+    iv = ImageViewer (parent_obj = sg.objs.root()
+                     ,picture    = '/home/pete/pics/240255-1366x768.jpg'
+                     )
+    iv.show()
+    sg.objs.end()
