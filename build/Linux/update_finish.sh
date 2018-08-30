c479f9c01ac500e381c98e491d2a789c1fa765ff
skl.progs <skl.progs@gmail.com>
2018-08-31 00:08:44 +0300

    Build scripts (Linux)

--- build/Linux/update_finish.sh
+++ build/Linux/update_finish.sh
@@ -2,7 +2,7 @@
 
 # Do this after testing the program
 
-rm -f $HOME/binaries/ImageViewer/linux.tar.bz2
-tar -cvjSf $HOME/binaries/ImageViewer/linux.tar.bz2 ./ImageViewer
+rm -f $HOME/binaries/ImageViewer/lin32.tar.bz2
+tar -cvjSf $HOME/binaries/ImageViewer/lin32.tar.bz2 ./ImageViewer
 rm -r ./ImageViewer
 ./clean_up.sh
---
+++ build/Linux/update_structure.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Do not use "verbose" in order to spot errors easily
+
+mkdir -p ./{resources/locale/ru/LC_MESSAGES/,src}
+
+# Copy shared resources
+cp -u /usr/local/bin/shared/resources/{error.gif,info.gif,question.gif,warning.gif} ./resources/
+
+# Copy ImageViewer image files
+cp -u /usr/local/bin/ImageViewer/resources/Gnu_\(PSF\).png ./resources/
+
+# Copy other ImageViewer resources
+cp -u /usr/local/bin/ImageViewer/resources/locale/ru/LC_MESSAGES/ImageViewer.mo ./resources/locale/ru/LC_MESSAGES/
+cp -u /usr/local/bin/ImageViewer/resources/icon_64x64_viewer.gif ./resources/
+
+# Copy ImageViewer Python files
+cp -u /usr/local/bin/ImageViewer/src/ImageViewer.py ./src/
+
+# Copy shared Python files
+cp -u /usr/local/bin/shared/src/{gettext_windows.py,shared.py,sharedGUI.py} ./src/
+
+ls --color=always .
